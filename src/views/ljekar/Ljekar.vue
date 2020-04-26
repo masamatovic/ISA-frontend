c6a8f0ce7fc9f6878fec5addfc56f8bd19a9e6cb
gitSrdzaan <srdjan_kralj_97@live.com>
2020-04-26 12:36:05 +0200

    ljekar forma za azuriranje podataka

---
+++ src/views/ljekar/Ljekar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="doctorHome">
+      <!--docor info-->
+      <form>
+          <div class="form-row">
+              <div class="form-group">
+                  <lable for="doctorName">Ime doktora: </lable>
+                  <input type="text" id="doctorName"/>
+              </div>
+          </div>
+          <div class="form-row">
+              <div class="form-group">
+                  <lable for="doctorSurname">Prezime doktora: </lable>
+                  <input type="text" id="doctorSurname"/>
+              </div>
+          </div>
+          <div class="form-row">
+              <div class="form-group">
+                  <lable for="doctorEmail">Email doktora: </lable>
+                  <input type="text" id="doctorEmail"/>
+              </div>
+          </div>
+          <div class="form-row">
+              <div class="form-group">
+                  <lable for="doctorAdress">Adresa doktora: </lable>
+                  <input type="text" id="doctorAdress"/>
+              </div>
+          </div>
+          <div class="form-row">
+              <div class="form-group">
+                  <lable for="doctorCity">Grad doktora: </lable>
+                  <input type="text" id="doctorName"/>
+              </div>
+          </div>
+          <div class="form-row">
+              <div class="form-group">
+                  <lable for="doctorState">Drzava doktora: </lable>
+                  <input type="text" id="doctorState"/>
+              </div>
+          </div>
+          <div class="form-row">
+              <div class="form-group">
+                  <lable for="doctorPhone">Telefon doktora: </lable>
+                  <input type="text" id="doctorPhone"/>
+              </div>
+          </div>
+          <div class="form-row">
+              <div class="form-group">
+                  <lable for="doctorJMBG">JMBG doktora: </lable>
+                  <input type="text" id="doctorJMBG"/>
+              </div>
+          </div>
+      </form>
+      <!--end form-->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
